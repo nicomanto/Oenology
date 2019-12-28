d4e68136737a4eb33d43df710c476a3502b9916b
Niccolò Mantovani <nicomanto49@gmail.com>
2019-12-28 10:48:49 +0100

    Aggiunta di altre query da provare

--- src/sql/IdeeQuery.sql
+++ src/sql/IdeeQuery.sql
@@ -1,6 +1,15 @@
+/*
+-Ogni query è stata fatta tenendo conto dell'aggiunta di ID per BottigliaDiVino come chiave primaria
+-Ogni query è da provare nel database perchè non sono sicuro della correttezza
+*/
+
+
+
+
 /*QUERY 1
 Bottiglia/e di vino più venduta/e dell'anno 2019 con la relativa quantita(Da vedere se è corretta, 
 si potrebbero usare le viste per rendere le cose più semplici)*/
+
 
 SELECT B.Nome,B.Annata, SUM(D.QuantitaBottiglia) as QuantitaVenduta
 FROM BottiglieDiVino as B, Dettagli as D, Ordine as O
@@ -36,3 +45,34 @@ ORDER BY Acq1.Nome, Acq2.Nome
 
 /*QUERY 4
 il vino/i che rappresenta il tema di un evento che è stato visto da meno partecipanti*/
+
+CREATE VIEW Num_partecipanti_evento as
+SELECT Evento as Cod_evento, SUM(Id) as Num_partecipanti
+FROM Partecipanti
+GROUP BY Evento
+
+
+
+SELECT V.Nome
+FROM Vino as V, TemiVino as T
+WHERE T.Vino=V.Id and T.Id IN
+
+(SELECT Cod_evento, 
+FROM Num_partecipanti_evento
+WHERE Num_partecipanti IN
+
+SELECT MIN(Num_partecipanti)
+FROM Num_partecipanti_evento))
+
+
+
+/*QUERY 5
+Lista dei dipendenti (nome, cognome), con inizio e fine turno, 
+ordinati in modo decrescente sull'inizio del turno,
+che sono a lavorare il giorno 12 giugno 2019 (giorno da decidere) */
+
+
+SELECT D.Nome, D.cognome, T.InizioTurno, T.FineTurno
+FROM Dipendenti as D, Turni as T
+WHERE T.Dipedente=D.CodiceFiscale AND DATE_FORMAT(T.InizioTurno, '%Y-%m-%d') ='2019-06-12'
+ORDER BY T.InizioTurno DESC
