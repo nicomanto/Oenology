bae51ae56fa5c1c235af114b27ab381f24671dc6
Niccolò Mantovani <nicomanto49@gmail.com>
2020-01-08 13:21:38 +0100

    Eliminazione commenti

--- src/sql/IdeeQuery.sql
+++ src/sql/IdeeQuery.sql
@@ -1,100 +1,66 @@
-/*
- -Ogni query è stata fatta tenendo conto dell'aggiunta di ID per BottigliaDiVino come chiave primaria
- -Ogni query è da provare nel database perchè non sono sicuro della correttezza
- */
 /*QUERY 1
- Bottiglia/e di vino più venduta/e dell'anno 2019 con la relativa quantita(Da vedere se è corretta, 
- si potrebbero usare le viste per rendere le cose più semplici)*/
+ Stampa resoconto settimanale fatturato.*/
+
+SELECT
+	SUM(Ordini.PrezzoTotale)
+FROM
+	Ordini
+WHERe
+	Ordini.Data <= CURRENT_TIMESTAMP
+	AND Ordini.Data >= DATE_ADD(CURRENT_TIMESTAMP, INTERVAL -7 DAY);
+
+
+/*QUERY 2
+ Bottiglia/e di vino più venduta/e dell'anno 2019 con la relativa quantità.
+Inizialmente viene creata una vista per definire le bottiglie vendute nell'anno 2019*/
+
 CREATE VIEW OrdineQuantita AS
-SELECT
-    Vini.Nome,
-    SUM(Dettagli.QuantitaBottiglie) AS BottiglieVendute
-FROM
-    (
-        SELECT
-            Dettagli.*
-        FROM
-            Dettagli,
-            Ordini
-        WHERE
-            DATE_FORMAT(Ordini.Data, '%Y') = '2019'
-            AND Ordini.Id = Dettagli.Ordine
-    ) AS Dettagli,
-    BottiglieDiVino,
-    Vini
+SELECT 
+	Vini.Nome, SUM(Dettagli.QuantitaBottiglie) AS BottiglieVendute
+FROM 
+	Dettagli, BottiglieDiVino, Vini, Ordini
 WHERE
-    Dettagli.BottigliaDiVino = BottiglieDiVino.Id
-    AND Vini.Nome = BottiglieDiVino.Vino
+	Dettagli.BottigliaDiVino = BottiglieDiVino.Id
+	AND Vini.Nome = BottiglieDiVino.Vino
+	AND DATE_FORMAT(Ordini.Data, '%Y') = '2019'
+	AND Ordini.Id = Dettagli.Ordine
 GROUP BY
-    Dettagli.BottigliaDiVino;
+	Dettagli.BottigliaDiVino;
 
-SELECT
-    OrdineQuantita.Nome,
-    OrdineQuantita.BottiglieVendute
-FROM
-    OrdineQuantita
-WHERE
-    OrdineQuantita.BottiglieVendute IN (
-        SELECT
-            MAX(OrdineQuantita.BottiglieVendute)
-        FROM
-            OrdineQuantita
-    );
-    /*QUERY 2
-     Numero di tipi di vino prodotti divisi per colore*/
-SELECT
-    SUM(Ordini.PrezzoTotale) AS FatturatoSettimanale
-FROM
-    Ordini
-WHERE
-    Ordini.Data <= CURRENT_TIMESTAMP
-    AND Ordini.Data >= DATE_ADD(CURRENT_TIMESTAMP, INTERVAL -7 DAY);
 
--- Per screenshot
--- SELECT SUM(Ordini.PrezzoTotale)
---     FROM Ordini
---     WHERE Ordini.Data <= '2016-01-24' AND Ordini.Data >= '2016-01-17';
+SELECT 
+	OrdineQuantita.Nome, OrdineQuantita.BottiglieVendute
+FROM
+	OrdineQuantita
+WHERE 
+	OrdineQuantita.BottiglieVendute IN (
+	SELECT 
+		MAX(OrdineQuantita.BottiglieVendute)
+	FROM 
+		OrdineQuantita);
+    
+
 /*QUERY 3
- Coppie di aziende che hanno acquistato lo stesso giorno, con il relativo giorno, in ordine alfabetico*/
-SELECT
-    Informazioni.Nome AS Azienda,
-    TipiUva.Nome AS TipoUva,
-    Vini.Nome AS Vino
+ Lista dei vini prodotti con la relativa tipologia di uva utilizzata e il fornitore di quest'ultima.*/
+SELECT DISTINCT
+	Vini.Nome AS Vino,
+	Uva.TipoUva as TipoUva,
+	Informazioni.Nome as Fornitore
 FROM
-    Vini,
-    Uva,
-    Informazioni,
-    Aziende,
-    TipiUva,
-    FornituraUva
+	Vini,
+	Uva,
+	Informazioni,
+	Aziende
 WHERE
-    Vini.Uva = Uva.Id
-    AND Uva.Fornitore = Aziende.PartitaIVA
-    AND Aziende.InformazioniAggiuntive = Informazioni.Id
-GROUP BY Azienda;
+	Vini.Uva = Uva.Id
+	AND Uva.Fornitore = Aziende.PartitaIVA
+	AND Aziende.InformazioniAggiuntive = Informazioni.Id;
 
-    -- SELECT
-    --     Acq1.Nome,
-    --     Acq2.Nome,
-    --     Acq1.Data
-    -- FROM
-    --     Acquirenti as Acq1
-    --     JOIN Aziende as A ON Acq1.Id = NumAcquirente
-    --     JOIN Ordini ON Acquirente = Acq1.Id
-    --     JOIN Informazioni ON InformazioniAggiuntive = Informazioni.Id,
-    --     Acquirenti as Acq2
-    --     JOIN Aziende as A ON Acq2.Id = NumAcquirente
-    -- )
-    -- JOIN Ordini ON Acquirente = Acq2.Id
-    -- JOIN Informazioni ON InformazioniAggiuntive = Informazioni.Id,
-    -- WHERE
-    --     Acq1.Id < Acq2.Id
-    --     and Acq1.Data = Acq2.Data
-    -- ORDER BY
-    --     Acq1.Nome,
-    --     Acq2.Nome
+
+
+
     /*QUERY 4
-     Lista dei dipendenti (ordinati in ordine alfabetico) che sono supervisori di altri dipendenti (se sono troppi limita le nuple a 10)*/
+     Lista dei dipendenti (ordinati in ordine alfabetico) che sono supervisori di altri dipendenti*/
 SELECT
     DISTINCT D1.Nome,
     D1.Cognome
@@ -107,10 +73,13 @@ ORDER BY
     D1.Nome,
     D1.Cognome;
 
+
+
+
 /*QUERY 5
  Lista dei dipendenti (nome, cognome), con inizio e fine turno, 
  ordinati in modo decrescente sull'inizio del turno,
- che sono a lavorare il giorno 12 giugno 2019 (giorno da decidere) */
+ che sono a lavorare il giorno 22 ottobre 2019 */
 SELECT
     D.Nome,
     D.cognome,
@@ -125,8 +94,12 @@ WHERE
 ORDER BY
     T.InizioTurno DESC;
 
+
+
+
 /* QUERY 6
- Lista degli acquirenti che hanno acquistato il maggiore valore di bottiglie di vino dalla nostra cantina
+ Lista degli acquirenti che hanno acquistato il maggiore valore (Prezzo) di bottiglie di vino dalla nostra cantina.
+Viene creata una vista che identifica le spese totali per ogni singolo acquirente
  */
 CREATE VIEW SpeseTotali AS
 SELECT
